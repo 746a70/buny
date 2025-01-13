1cbf7d84b10aaa2bcf0b50800f39146b1536d575
746a70 <terrell.paiva@gmail.com>
2025-01-12 19:56:55 -0800

    changes 2

--- components/footer.js
+++ components/footer.js
@@ -5,6 +5,10 @@ class Footer extends HTMLElement {
     connectedCallback() {
       this.innerHTML = `
         <footer>
+          <a href="https://instagram.com/buny________"><img src="images/ig.png" width="100"></a>
+          <a href="https://soundcloud.com/djbuny"><img src="images/sc.png" width="100"></a>
+          <a href="https://bunyy.bandcamp.com"><img src="images/bc.png" width="100"></a>
+          <a href="https://ra.co/dj/buny"><img src="images/ra.png" width="100" margin-bottom="-100"></a>
           <p>© 2024 buny's world</p>
         </footer>
       `;
