79b639675ee84a4b3b41806c46a0b85412e5ae79
746a70 <terrell.paiva@gmail.com>
2025-01-12 20:09:46 -0800

    copyright year change

--- components/footer.js
+++ components/footer.js
@@ -8,8 +8,8 @@ class Footer extends HTMLElement {
           <a href="https://instagram.com/buny________"><img src="images/ig.png" width="100"></a>
           <a href="https://soundcloud.com/djbuny"><img src="images/sc.png" width="100"></a>
           <a href="https://bunyy.bandcamp.com"><img src="images/bc.png" width="100"></a>
-          <a href="https://ra.co/dj/buny"><img src="images/ra.png" width="100" margin-bottom="-100"></a>
-          <p>© 2024 buny's world</p>
+          <a href="https://ra.co/dj/buny"><img src="images/ra.png" width="100"></a>
+          <p>© 2025 buny's world</p>
         </footer>
       `;
     }
